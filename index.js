a7dc0f95fd067b538445350030a98859b3a6c34a
Gaukhar <gauhar.abdizhapbarova@gmail.com>
2022-03-13 17:50:32 +0600

    Add game() function
    game() function calls a playRound() function 5 times to play a 5 round game and at the end reports the result and winner of the game

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
 function computerPlay() {
     let list = ['Rock','Paper','Scissors'];
     let randomSelection = list[Math.floor(Math.random() * list.length)];
-    alert(randomSelection);
+    alert("Computer's choice: " + randomSelection);
+    return randomSelection;
     
 }
+
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "The result was a tie!";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore++;
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++;
+        return "You Win! Paper beats Rock!";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore++;
+        return "You Lose! Rock beats Scissors!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++;
+        return "You Win! Rock beats Scissors!";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore++;
+        return "You Lose! Scissors beats Paper!";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++;
+        return "You Win! Scissors beats Paper!";
+    } else {
+        return "Incorrect input";
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Your choice?").toLowerCase();
+        const computerSelection = computerPlay().toLowerCase();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    alert("Your score is " + playerScore + " and the computer's score is " + computerScore);
+    if (playerScore > computerScore) {
+        alert("You won this game!")
+    } else if (computerScore > playerScore) {
+        alert("Computer won this game!")
+    } else if (computerScore === playerScore) {
+        alert("There is no winner, try again!")
+    }
+}
+
+game();
